4d90d2c81fe7c8718930fac4ce84e1f9828e80e0
anduser <makarovalexander1@yandex.ru>
2024-07-03 22:52:52 +0300

    test2

--- src/main/java/com/example/demo/string/lostupdate/TestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.string.lostupdate;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestRepository extends CrudRepository<Order, Long> {
-
-}
